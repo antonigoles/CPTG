0d88d9af24487ecfc9ed07f1c71a3e04ac3480a4
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-02-20 19:03:05 +0100

    included boost libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/foreach.hpp>
+
 int main()
 {
     std::cout << "it lives\n";
