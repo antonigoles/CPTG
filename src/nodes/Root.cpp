7eefb9ec745a2b3ba128e9f1dfd7a776b9371c76
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-02-21 21:35:25 +0100

    Temporary implementation for virtual functions

--- src/nodes/Root.cpp
+++ src/nodes/Root.cpp
@@ -1,1 +1,21 @@
 #include "Root.hpp"
+
+Root::Root()
+{
+    std::cout << "root construct\n";
+}
+
+void Root::setRootTag(std::shared_ptr< boost::property_tree::ptree > rootTag)
+{
+    std::cout << "root tag set!\n";
+}
+
+void Root::FindSubNodes()
+{
+
+}
+
+void Root::Print()
+{
+    
+}
