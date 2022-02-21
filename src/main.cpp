e0bc8b24437eab47a198de06fe88e8c6a12bcd81
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-02-21 21:36:53 +0100

    Find test tag and send it to root object

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
+#include "nodes/Root.hpp"
+
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/foreach.hpp>
 
 int main()
 {
-    std::cout << "it lives\n";
+    Root root;
+    boost::property_tree::ptree tree;
+    boost::property_tree::read_xml("example1.xml", tree);
 
-    return 0;
+    tree.get_child("test");
+
+    // Iterate through base tags
+    BOOST_FOREACH(boost::property_tree::ptree::value_type& child, tree)
+    {   
+        if(child.first == "test")
+        {
+            root.setRootTag(
+                std::make_shared< boost::property_tree::ptree >(child.second)
+                );
+        }
+    }
 }
