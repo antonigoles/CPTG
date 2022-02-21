0462c65821a89a2bce0603958f412e8b7cbfddf1
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-02-21 21:34:52 +0100

    Declared variables for root and setter for rootTag

--- src/nodes/Root.hpp
+++ src/nodes/Root.hpp
@@ -1,8 +1,23 @@
 #pragma once
 
+#include <vector>
+#include <memory>
+#include <iostream>
+
 #include "Node.hpp"
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/foreach.hpp>
 
 class Root : public Node
 {
+    std::shared_ptr< boost::property_tree::ptree > rootTag;
+    std::vector< std::shared_ptr< Node > > subnodes;
 
+public:
+    Root();
+    void FindSubNodes() override;
+    void Print() override;
+
+    void setRootTag(std::shared_ptr< boost::property_tree::ptree > rootTag);
 };
