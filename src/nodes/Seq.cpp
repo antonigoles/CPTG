b5a779171646cef9ccd8291cbb4ccb1378b27750
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-02-26 21:08:15 +0100

    Replaced Root class with Seq class (this was basicly the same code)

--- src/nodes/Seq.cpp
+++ src/nodes/Seq.cpp
@@ -1,1 +1,30 @@
 #include "Seq.hpp"
+
+Seq::Seq() { }
+
+Seq::Seq(std::shared_ptr< boost::property_tree::ptree > sT) : 
+    seqTag(sT),
+    times(seqTag->get<int>("<xmlattr>.times"))
+{
+
+}
+
+Seq::Seq(std::shared_ptr< boost::property_tree::ptree > sT, int times) : 
+    seqTag(sT),
+    times(times)
+{
+
+}
+
+void Seq::FindSubNodes()
+{
+    BOOST_FOREACH(boost::property_tree::ptree::value_type& child, (*seqTag))
+    {
+        std::cout << child.first << '\n';
+    }
+}
+
+void Seq::Print()
+{
+
+}
