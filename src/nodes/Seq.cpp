189bfea90b36d0f914b2efd6eb2e50d40db1823e
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-03-08 12:41:31 +0100

    Created macro replacing long shared pointer type declarations

--- src/nodes/Seq.cpp
+++ src/nodes/Seq.cpp
@@ -1,5 +1,11 @@
 #include "Seq.hpp"
 
+// Creates a shared pointer to nodeType object
+#define SharedNode(nodeType, propTree) \
+    std::make_shared<nodeType>( \
+        nodeType(std::make_shared<boost::property_tree::ptree>(propTree)) \
+    )
+
 Seq::Seq() : times(1) { }
 
 Seq::Seq(std::shared_ptr< boost::property_tree::ptree > sT) : 
@@ -11,33 +17,25 @@ Seq::Seq(std::shared_ptr< boost::property_tree::ptree > sT) :
 
 Seq::Seq(std::shared_ptr< boost::property_tree::ptree > sT, int times) : 
     seqTag(sT),
-    times(times)
-{
-
-}
+    times(times) { }
 
 void Seq::FindSubNodes()
 {
     BOOST_FOREACH(boost::property_tree::ptree::value_type& child, (*seqTag))
-    {
-        //std::cout << child.first << '\n';
+    {        
         if(child.first == "seq")
         {
-            subnodes.push_back(std::make_shared<Seq>(
-                Seq(std::make_shared<boost::property_tree::ptree>(child.second))
-            ));
+            subnodes.push_back( SharedNode(Seq, child.second) );
         }
 
         if(child.first == "const")
         {
-            subnodes.push_back(std::make_shared<Const>(
-                Const(std::make_shared<boost::property_tree::ptree>(child.second))
-            ));
+            subnodes.push_back( SharedNode(Const, child.second) );
         }
 
         if(child.first == "br")
         {
-            subnodes.push_back(std::make_shared<Br>(Br()));
+            subnodes.push_back( std::make_shared<Br>() );
         }
     }
 }
