61470510d507ceb70fd6520c84526ee5698bc816
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-03-10 18:53:16 +0100

    Tag constructor implementation

--- src/nodes/Var.cpp
+++ src/nodes/Var.cpp
@@ -6,12 +6,34 @@ Var::Var(std::shared_ptr< boost::property_tree::ptree > tag) :
     varTag(tag),
     varType(Type::Number)
 {
+    // Pointer created to attributes node only if they exist
+    boost::optional< boost::property_tree::ptree& > child =
+        varTag->get_child_optional("<xmlattr>");
 
-}
+    if(child)
+    {
+        if(child.value().count("type"))
+        {
+            // TODO: what if the entered type does not exist?
+            varType = typeMap[varTag->get<std::string>("<xmlattr>.type")];
+        }
+    }
 
-void Var::FindNumericRange()
-{
-
+    // Find the range for var variable
+    if(varType == Type::Number || varType == Type::Float)
+    {
+        FindNumericRange();
+    }
+    else
+    {
+        FindLexicalRange();
+    }
+    
+    // Find lengh only if needed
+    if(varType == Type::String || varType == Type::Float)
+    {
+        FindLength();
+    }
 }
 
 void Var::Print()
