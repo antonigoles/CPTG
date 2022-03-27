91114077002fbd9e040231992ccd1dd9d499c6e2
Bartlomiej Sefanski <bartex00001@gmail.com>
2022-03-27 18:51:59 +0200

    Refactor - use early returns istead of nested if's

--- src/nodes/Var.cpp
+++ src/nodes/Var.cpp
@@ -49,100 +49,95 @@ void Var::FindParameters(const boost::optional< boost::property_tree::ptree& >& 
 // Returns true if type is valid, type can be empty
 bool Var::FindType(const boost::optional< boost::property_tree::ptree& >& attributes)
 {
-    if(attributes.value().count("type"))
+    if(!attributes.value().count("type"))
     {
-        // If type is not in typemap, return false
-        if(typeMap.find(attributes.value().get<std::string>("type")) == typeMap.end())
-        {
-            return false;
-        }
-
-        varType = typeMap[varTag->get<std::string>("<xmlattr>.type")];
+        return true;
     }
 
+    // If type is not in typemap, return false
+    if(typeMap.find(attributes.value().get<std::string>("type")) == typeMap.end())
+    {
+        return false;
+    }
+
+    varType = typeMap[varTag->get<std::string>("<xmlattr>.type")];
     return true;
 }
 
 void Var::FindNumericRange(const boost::optional< boost::property_tree::ptree& >& attributes)
 {
-    // if no range parameter is specified, return
-    if(attributes.value().count("range"))
-    {
-        std::string rangeStr = attributes.value().get<std::string>("range");
-        std::vector< std::string > splitRange;
-
-        boost::split(splitRange, rangeStr, boost::is_any_of(":"));
-
-        if(std::stoi(splitRange[0]) > std::stoi(splitRange[1]))
-        {
-            std::cout << "Error: Range is not valid\nUsing default instead" << std::endl;
-            range = {0, 9};
-            return;
-        }
-
-        range = {std::stoi(splitRange[0]), std::stoi(splitRange[1])};
-    }
-    else
+    if(!attributes.value().count("range"))
     {
         range = {0, 9};
+        return;
     }
+
+    std::string rangeStr = attributes.value().get<std::string>("range");
+    std::vector< std::string > splitRange;
+
+    boost::split(splitRange, rangeStr, boost::is_any_of(":"));
+
+    if(std::stoi(splitRange[0]) > std::stoi(splitRange[1]))
+    {
+        std::cout << "Error: Range is not valid\nUsing default instead" << std::endl;
+        range = {0, 9};
+        return;
+    }
+
+    range = {std::stoi(splitRange[0]), std::stoi(splitRange[1])};
 }
 
 void Var::FindLexicalRange(const boost::optional< boost::property_tree::ptree& >& attributes)
 {
-    // if no range parameter is specified, set default lexical range and return
-    if(attributes.value().count("range"))
+    if(!attributes.value().count("range"))
     {
-        std::string rangeKey = varTag->get<std::string>("<xmlattr>.range");
+        range = {'a', 'z'};
+        return;
+    }
 
-        if(rangeKey == "abc")
-        {
-            range = {'a', 'z'};
-        }
-        if(rangeKey == "ABC")
-        {
-            range = {'A', 'Z'};
-        }
-        else if(rangeKey == "special")
-        {
-            // TODO: range does not cover all characters
-            range = {':', '@'};
-        }
-        else if(rangeKey == "all")
-        {
-            range = {'!', '~'};
-        }
-        else
-        {
-            std::cout << "Error: Unknown range\nUsed default instead" << std::endl;
-            range = {'a', 'z'};
-        }
+    std::string rangeKey = varTag->get<std::string>("<xmlattr>.range");
+
+    if(rangeKey == "abc")
+    {
+        range = {'a', 'z'};
+    }
+    if(rangeKey == "ABC")
+    {
+        range = {'A', 'Z'};
+    }
+    else if(rangeKey == "special")
+    {
+        // TODO: range does not cover all characters
+        range = {':', '@'};
+    }
+    else if(rangeKey == "all")
+    {
+        range = {'!', '~'};
     }
     else
     {
-        // Set default range for lexical variables
+        std::cout << "Error: Unknown range\nUsed default instead" << std::endl;
         range = {'a', 'z'};
     }
 }
 
 void Var::FindLength(const boost::optional< boost::property_tree::ptree& >& attributes)
 {
-    if(attributes.value().count("length"))
+    if(!attributes.value().count("length"))
     {
-        int readLength = varTag->get<int>("<xmlattr>.length");
+        length = 3;
+        return;
+    }
+
+    int readLength = varTag->get<int>("<xmlattr>.length");
         
-        if(readLength >= 0)
-        {
-            length = readLength;
-        }
-        else
-        {
-            std::cout << "Error: Length must be positive\nUsing default length instead" << std::endl;
-            length = 3;
-        }
+    if(readLength >= 0)
+    {
+        length = readLength;
     }
     else
     {
+        std::cout << "Error: Length must be positive\nUsing default length instead" << std::endl;
         length = 3;
     }
 }
